85f6a58ca052e64595dc69e593ca4aee51b9f92e
Jack Malone <c00236428@itcarlow.ie>
2022-01-20 20:50:16 +0000

    Finished project

--- FuzzyLogic/Game.cpp
+++ FuzzyLogic/Game.cpp
@@ -3,9 +3,52 @@
 
 
 Game::Game() :
-	m_window{ sf::VideoMode{ 800U, 600U, 32U }, "SFML Game" },
-	m_exitGame{ false }
+	m_window{ sf::VideoMode{ 1000U, 600U, 32U }, "SFML Game" },
+	m_exitGame{ false }, m_dividingLine{sf::Lines, 2}
 {
+	if (!m_font.loadFromFile("assets//fonts//MONOFONT.ttf"))
+	{
+		std::cerr << "Unable to load font" << std::endl;
+		throw std::exception("File not found");
+	}
+
+	if (!m_texture.loadFromFile("assets//images//background.jpg"))
+	{
+		std::cerr << "Unable to load background" << std::endl;
+		throw std::exception("File not found");
+	}
+
+	m_background.setTexture(m_texture);
+
+	m_numberOfFriendlies.setFont(m_font);
+	m_numberOfFriendlies.setCharacterSize(18);
+	m_numberOfFriendlies.setPosition(sf::Vector2f(100, 50));
+	m_numberOfFriendlies.setFillColor(sf::Color::Black);
+
+	m_numberOfEnemies.setFont(m_font);
+	m_numberOfEnemies.setCharacterSize(18);
+	m_numberOfEnemies.setPosition(sf::Vector2f(300, 50));
+	m_numberOfEnemies.setFillColor(sf::Color::Black);
+
+	m_distanceAwayOfEnemies.setFont(m_font);
+	m_distanceAwayOfEnemies.setCharacterSize(18);
+	m_distanceAwayOfEnemies.setPosition(sf::Vector2f(500, 50));
+	m_distanceAwayOfEnemies.setFillColor(sf::Color::Black);
+
+	m_textBackground.setSize(sf::Vector2f(625, 75));
+	m_textBackground.setPosition(m_numberOfFriendlies.getPosition() - sf::Vector2f(25, 25));
+	m_textBackground.setFillColor(sf::Color(211, 211, 211, 125));
+	m_dividingLine.append(sf::Vertex(sf::Vector2f(100, 0), sf::Color::Black));
+	m_dividingLine.append(sf::Vertex(sf::Vector2f(100, 600), sf::Color::Black));
+
+	m_guide.setFont(m_font);
+	m_guide.setCharacterSize(18);
+	m_guide.setPosition(sf::Vector2f(800, 50));
+	m_guide.setFillColor(sf::Color::Black);
+	m_guide.setString("Press Enter to reset");
+	m_guideBackground.setSize(sf::Vector2f(200, 75));
+	m_guideBackground.setPosition(m_guide.getPosition() - sf::Vector2f(15, 25));
+	m_guideBackground.setFillColor(sf::Color(211, 211, 211, 125));
 	setUpEnemies();
 }
 
@@ -58,6 +101,11 @@ void Game::processKeys(sf::Event t_event)
 	{
 		m_exitGame = true;
 	}
+
+	if (sf::Keyboard::Enter == t_event.key.code)
+	{
+		setUpMatrix();
+	}
 }
 
 
@@ -73,7 +121,24 @@ void Game::update(sf::Time t_deltaTime)
 void Game::render()
 {
 	m_window.clear(sf::Color::White);
+	m_window.draw(m_background);
 
+	for (auto& friendly : m_friendlies)
+	{
+		m_window.draw(friendly);
+	}
+
+	for (auto& enemy : m_enemies)
+	{
+		m_window.draw(enemy);
+	}
+	m_window.draw(m_textBackground);
+	m_window.draw(m_numberOfFriendlies);
+	m_window.draw(m_numberOfEnemies);
+	m_window.draw(m_distanceAwayOfEnemies);
+	m_window.draw(m_guideBackground);
+	m_window.draw(m_guide);
+	m_window.draw(m_dividingLine);
 	m_window.display();
 }
 
@@ -84,50 +149,81 @@ void Game::setUpEnemies()
 
 void Game::setUpMatrix()
 {
-	int enemySize = rand() % 8 + 1;
-	int distance = rand() % 65 + 10;
+	numberOfEnemies = rand() % 30 + 1;
+	distanceAwayOfEnemies = rand() % 65 + 10;
 	Fzzy::Fuzzifier::ruleMatrix ruleMatrix;
 	Fzzy::Fuzzifier::triangle tri;
-	tri.value = enemySize;
+	tri.value = numberOfEnemies;
 	tri.x0 = -10;
 	tri.x1 = 0;
 	tri.x2 = 10;
 	ruleMatrix.m_tiny = tri;
 	Fzzy::Fuzzifier::trapezoid trap;
-	trap.value = enemySize;
+	trap.value = numberOfEnemies;
 	trap.x0 = 2.5;
 	trap.x1 = 10;
 	trap.x2 = 15;
 	trap.x3 = 20;
 	ruleMatrix.m_small = trap;
-	trap.value = enemySize;
+	trap.value = numberOfEnemies;
 	trap.x0 = 15;
 	trap.x1 = 20;
 	trap.x2 = 25;
 	trap.x3 = 30;
 	ruleMatrix.m_moderate = trap;
 	Fzzy::Fuzzifier::grade gr;
-	gr.value = enemySize;
+	gr.value = numberOfEnemies;
 	gr.x0 = 25;
 	gr.x1 = 30;
 	ruleMatrix.m_large = gr;
-	tri.value = distance;
+	tri.value = distanceAwayOfEnemies;
 	tri.x0 = -30;
 	tri.x1 = 0;
 	tri.x2 = 30;
 	ruleMatrix.m_close = tri;
-	trap.value = distance;
+	trap.value = distanceAwayOfEnemies;
 	trap.x0 = 10;
 	trap.x1 = 30;
 	trap.x2 = 50;
 	trap.x3 = 70;
 	ruleMatrix.m_medium = trap;
-	gr.value = distance;
+	gr.value = distanceAwayOfEnemies;
 	gr.x0 = 50;
 	gr.x1 = 70;
 	ruleMatrix.m_far = gr;
 	Fzzy::Fuzzifier::SetUpRuleMatrix(ruleMatrix);
-	m_deploy = Fzzy::Fuzzifier::DefuzzifyRuleMatrix(ruleMatrix);
-	std::cout << m_deploy << std::endl;
+	m_deploy = std::ceil(Fzzy::Fuzzifier::DefuzzifyRuleMatrix(ruleMatrix));
+	m_numberOfFriendlies.setString("Number of friendlies: " + std::to_string(m_deploy));
+	m_numberOfEnemies.setString("Number of enemies: " + std::to_string(numberOfEnemies));
+	m_distanceAwayOfEnemies.setString("Distance from Enemies: " + std::to_string(distanceAwayOfEnemies));
+	setUpDisplay();
+}
+
+void Game::setUpDisplay()
+{
+	m_friendlies.clear();
+	m_enemies.clear();
+
+	for (size_t i = 0; i < m_deploy; ++i)
+	{
+		int x = rand() % 90 + 4;
+		int y = rand() % 590 + 4;
+		sf::CircleShape shape;
+		shape.setRadius(5);
+		shape.setPosition(x, y);
+		shape.setFillColor(sf::Color::Blue);
+		m_friendlies.push_back(shape);
+	}
+
+	for (size_t i = 0; i < numberOfEnemies; ++i)
+	{
+		int x = rand() % 200 + (distanceAwayOfEnemies * 10);
+		int y = rand() % 590 + 4;
+		sf::CircleShape shape;
+		shape.setRadius(5);
+		shape.setPosition(x, y);
+		shape.setFillColor(sf::Color::Red);
+		m_enemies.push_back(shape);
+	}
 }
 
--- FuzzyLogic/Game.h
+++ FuzzyLogic/Game.h
@@ -3,15 +3,15 @@
 
 #include <SFML/Graphics.hpp>
 #include "Fzzy.h"
+#include <vector>
+
 
 class Game
 {
 public:
 	Game();
 	~Game();
-
 	void run();
-
 private:
 
 	void processEvents();
@@ -21,6 +21,8 @@ private:
 
 	void setUpEnemies();
 	void setUpMatrix();
+
+	void setUpDisplay();
 
 	double m_tiny;
 	double m_small;
@@ -32,11 +34,29 @@ private:
 	double m_low;
 	double m_medium;
 	double m_high;
-	double m_deploy;
+	int m_deploy;
 	sf::RenderWindow m_window;
-
+	int numberOfEnemies;
+	int distanceAwayOfEnemies;
 	bool m_exitGame;
 
+	sf::Text m_numberOfFriendlies;
+	sf::Text m_numberOfEnemies;
+	sf::Text m_distanceAwayOfEnemies;
+
+	sf::Text m_guide;
+	sf::RectangleShape m_guideBackground;
+
+	sf::RectangleShape m_textBackground;
+	sf::Font m_font;
+
+	sf::Sprite m_background;
+	sf::Texture m_texture;
+
+	std::vector<sf::CircleShape> m_friendlies;
+	std::vector<sf::CircleShape> m_enemies;
+
+	sf::VertexArray m_dividingLine;
 };
 
 #endif 
